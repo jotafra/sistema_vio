b4f653a215ae26a873fb53806dd59bd6b0c1edda
jotafra <jozinbrbrasil@gmail.com>
2025-05-19 12:52:41 -0300

    19/05'

--- api/src/routes/apiRoutes.js
+++ api/src/routes/apiRoutes.js
@@ -10,7 +10,7 @@ const ingressoController = require('../controllers/ingressoController');
 //Rotas userController
 router.post('/user', userController.createUser);
 router.post('/login', userController.loginUser);
-//router.get('/user', userController.getAllUsers);
+router.get('/user', userController.getAllUsers);
 //router.get('/user/:cpf', userController.getxUserById); 
 router.put('/user', userController.updateUser);
 router.delete('/user/:id', userController.deleteUser);
